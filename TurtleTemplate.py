016c49eb622cefc34b2a425a54aeac186714f9e9
hundredvisionsguy <cwinikka@gmail.com>
2017-03-10 10:36:08 -0800

    adding template and grid

---
+++ TurtleTemplate.py
@@ -0,0 +1,24 @@
+# TurtleTemplate.py
+# by Hundred Visions Guy
+# A template for future turtle graphics
+
+# Import Statements
+from turtle import *
+import time
+
+# Set the title Screen
+title("Program Title Goes Here")
+
+# Create 1 pen
+p1 = Pen()  # pen 1
+
+# Set the colors
+p1.color('#2c95b5', 'gray')
+bgcolor('#101010')
+
+# Draw your picture below...
+
+
+# Pause to show the picture, then quit
+time.sleep(10)
+bye()
